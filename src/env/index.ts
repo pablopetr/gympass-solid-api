56c174f03d9f9c4f26abe6ea7ba80c3032b230c5
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-11-12 21:48:27 -0300

    Create authentication with JWT

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
   HOST: z.string().default('0.0.0.0'),
+  JWT_SECRET: z.string().default('secret'),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { register } from '@/http/controllers/register'
 import { authenticate } from '@/http/controllers/authenticate'
 import { FastifyInstance } from 'fastify'
+import { profile } from '@/http/controllers/profile'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
