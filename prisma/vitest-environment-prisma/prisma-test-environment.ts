6ad9412fc9dda1e9666d3dcbbb57a6706045d7b9
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-11-14 22:49:34 -0300

    Prepare project to create tests end-to-end with postgres random schemas

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,46 @@
+import 'dotenv/config'
+
+import { Environment } from 'vitest'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { prisma } from '@/lib/prisma'
+import { Schema } from 'zod'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default <Environment>(<unknown>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    return {
+      async teardown() {
+        const schema = randomUUID()
+        const databaseURL = generateDatabaseURL(schema)
+
+        process.env.DATABASE_URL = databaseURL
+
+        execSync('npx prisma migrate deploy')
+
+        return {
+          async tearDown() {
+            await prisma.$executeRawUnsafe(
+              `DROP SCHEMA IF EXISTS ${schema} CASCADE`,
+            )
+
+            await prisma.$disconnect()
+          },
+        }
+      },
+    }
+  },
+})
