d8511ccea35192e9d419b7764b2a3000ba359972
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-11-25 19:51:37 -0300

    Add github actions

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -7,6 +7,7 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
 
   const token = await reply.jwtSign(
     { role },
+
     {
       sign: {
         sub: request.user.sub,
